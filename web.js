66682c80cf8226fb6fc84e62597c64d361b901d5
vickybird <victoria.er.bird@gmail.com>
2014-05-13 16:05:05 +0100

    Starting a new project.

---
+++ web.js
@@ -0,0 +1,26 @@
+var keystone = require('keystone');
+keystone.init({
+
+  'name': 'Practice CMS',
+
+  'favicon': 'public/favicon.ico',
+  'static': 'public',
+
+  'views': 'templates/views',
+  'view engine': 'jade',
+
+  'auto update': true,
+  'mongo': 'mongodb://localhost/keystonetest',
+
+  'session': true,
+  'auth': true,
+  'user model': 'User',
+  'cookie secret': 'some long string for a secret'
+
+});
+
+require('/.models');
+
+keystone.set('routes', require('./routes'));
+
+keystone.start();
